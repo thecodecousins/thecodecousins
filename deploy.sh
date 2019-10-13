cc3e05a82e31b9460253ce6f6c1750f50f97c105
Stanley Nguyen <hung.ngn.the@gmail.com>
2019-10-13 18:51:17 +0800

    Add Travis build config

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 # If a command fails then the deploy stops
 set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
 # Go To Public folder
 cd public
+
+# authenticate bot with GitHub
+if [ -n "$GITHUB_AUTH_SECRET" ]
+then
+    touch ~/.git-credentials
+    chmod 0600 ~/.git-credentials
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+    git config credential.helper store
+    git config user.email "stanleynguyen-hugo-bot@users.noreply.github.com"
+    git config user.name "stanleynguyen-hugo-bot"
+fi
 
 # Add changes to git.
 git add .
@@ -22,4 +30,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+git push -f origin HEAD:master
